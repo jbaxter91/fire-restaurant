f8dc6afedb26ee5e534ccdb1ace50cf985cce6bd
John Baxter <jbaxter@a-zdarts.com>
2020-09-10 19:54:55 -0700

    Added some server routes, built out some index template

--- src/app.js
+++ src/app.js
@@ -2,15 +2,32 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
-app.use(express.static(path.join(__dirname, 'static')));
-
 var app = express();
 var PORT = process.env.PORT || 9000;
 
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// ============ GET Routes==========
+app.get("/api/tables",(res, req)=>{
+    res.sendFile("tableData.js")
+});
+
+app.get("/api/waitlist",(res, req)=>{
+    
+});
+
+app.get("/reserve",(res, req)=>{
+    
+});
+
+app.get("/table",(res, req)=>{
+    
+});
+// ==============================
+
 app.listen(PORT, () => 
 {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on: http://localhost:${PORT}`);
 })
