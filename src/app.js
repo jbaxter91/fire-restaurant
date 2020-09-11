1cc2dbaafb086e3e9a6b12f8291241ff83fdda99
John Baxter <jbaxter@a-zdarts.com>
2020-09-10 21:22:27 -0700

    Tried to add delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const tableData = require("./static/tableData");
-const waitingListData = require("./static/waitingListData");
+let tableData = require("./static/tableData");
+let waitingListData = require("./static/waitingListData");
 const { table } = require("console");
+const { filter } = require("./static/waitingListData");
 
 const MAX_TABLES = 5;
 const MAX_WAITLIST = 20;
@@ -34,20 +35,44 @@ app.get("/table", (req, res) => {
 // ==============================
 
 // ======== POST Requests ==========
-app.post("/api/add", function (req, res) {
+app.post("/", function (req, res) {
   var message = req.body;
   tableData.length <= MAX_TABLES ? addTable(message) : addWaitingList(message);
   res.send("All good");
 });
 // =================================
 
+// ========= DELETE Requests ========
+app.delete("/table", function (req, res) {
+    var customerID = req.body.customerID;
+    console.log("Trying to delete", customerID);
+    var removedList = tableData.filter( patron  => {
+        if(patron.customerID != customerID)
+        { 
+            return patron;
+        }
+    });
+
+    console.log(removedList);
+    tableData = removedList;
+    if(tableData.length < MAX_TABLES)
+    {
+        console.log("Before", waitingListData);
+        addTable(waitingListData.shift());
+        console.log("After", waitingListData);
+    }
+    res.send("All good");
+  });
+// 
+
 app.listen(PORT, () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 });
 
 function addTable(data) {
-  console.log("Adding Table");
+  console.log("Adding Table", data);
   tableData.push(data);
+  console.log(tableData);
 }
 
 function addWaitingList(data) {
